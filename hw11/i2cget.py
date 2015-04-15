55e0fc2c942dfa0e3531f70cef5f196341d30dd8
jordanm1653 <jordanm1653@gmail.com>
2015-04-15 11:29:12 -0400

    Update i2cget.py
    Added comments

--- hw11/i2cget.py
+++ hw11/i2cget.py
@@ -14,19 +14,28 @@ from collections import namedtuple
 
 bus = smbus.SMBus(1)	# 1 = /dev/i2c-1
 
+#
 # i2c Address of xmega
+#
 addr = 0x41
 
+#
 # Read 10 bytes from xmega
+#
 try:
 	data = bus.read_i2c_block_data(addr,0x00,10)
 except IOError:
 	print "unable to read from i2c bus"
 
+#
+# Format data from i2c read
+#
 buff= struct.unpack('<HHHHBB', struct.pack('BBBBBBBBBB', *data))
-print buff
 
+#
+# Create dictionary from i2c data
+#
 keys = ['roomcal','hotcal','adcal','v','room','hot']
-
 temp_t = dict(zip(keys, buff))
+
 print temp_t['v']
