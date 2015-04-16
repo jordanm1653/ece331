968c6a033427e50ba141f5db6eec08f8633e5cee
Jordan Millington <jordanm1653@gmail.com>
2015-04-15 20:17:56 -0400

    bode.py: Ready for submission

--- hw11/bode.py
+++ hw11/bode.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
 # bode.py
 #
 # Create a Bode plot of transfer funtion:
-
-#	H(s) = 8000s/(s^2+5000s+10^6
-# 	where: s = jw from 10 to 10k rad/s
+#
+#	              8000s
+#	G(s) = --------------------
+#	        s^2 + 5000s + 10^6
+#
+#	where: s = jw from 10 to 10k rad/sec
 # 
 # PACKAGES:
-#	apt-get install python-scypi
-#	apt-get install python-matplotlib
-#	apt-get install python-tk
+#	 python-scypi
+#	 python-matplotlib
+#	 python-tk
 #
 # AUTHOR: Jordan Millington
 
+#
+# Imports
+#
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
+#
+# Transfer function as LTI
+#
 sig = signal.lti([8000],[1, 5000, 10^6])
-w, mag, phase = signal.bode(sig)
 
+#
+# Frequency range & Bode magnitude
+# 	bode() returns magnitude in dB
+#
+w = np.logspace(1,4,100)
+w, mag, phase = signal.bode(sig,w)
+
+#
+# Plot setup
+#
 plt.figure()
 plt.semilogx(w, mag)
-plt.figure()
-plt.semilogx(w, phase)
+plt.title('Bode Plot of G(s) = 8000s / (s^2 + 5000s + 10^6)')
+plt.xlabel('Frequency [rad/sec]')
+plt.ylabel('Amplitude [dB]')
+plt.grid(axis='both',which='both')
 plt.show()
